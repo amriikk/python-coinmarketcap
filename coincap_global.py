af8df6eea744ecfe3c59214cdf9e22c006c939f3
John Trujillo <amriikk@gmail.com>
2021-01-20 00:35:52 -0800

    edit coinmarketcap

--- coincap_global.py
+++ coincap_global.py
@@ -1,9 +1,14 @@
-# import json
-import requests
+#The CoinMarketCap API -- Python3
 
-global_url = 'https://api.coinmarketcap.com/v2/global/'
+import json
+import requests
+from datetime import datetime
+
+currency = 'JPY'
+
+global_url = 'https://api.coinmarketcap.com/v2/global/?convert=' + currency
 
 request = requests.get(global_url)
 results = request.json()
 
-print(json.dumps(results, sort_keys=True, indent=4))
+# print(json.dumps(results, sort_keys=True, indent=4))
